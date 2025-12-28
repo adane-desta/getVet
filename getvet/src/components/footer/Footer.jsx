0469e9913a99f47ceba29da257b08d0c4d60ac83
adane-desta <addonegn@gmail.com>
2025-12-28 11:53:48 +0300

    organize files

--- getvet/src/components/footer/Footer.jsx
+++ getvet/src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 
-
+import style from './Footer.module.css'
 function Footer() {
 
     return (
@@ -7,28 +7,28 @@ function Footer() {
         <div>
 
             {/* Footer */}
-            <footer className="footer">
-                <div className="footerContainer">
-                    <div className="footerSection">
-                        <div className="footerLogo">
-                            <i className="fas fa-heartbeat"></i>
+            <footer className={style.footer}>
+                <div className={style.footerContainer}>
+                    <div className={style.footerSection}>
+                        <div className={style.footerLogo}>
+                         <i className={`${style.fas} ${style.faHeartbeat}`}></i>
                             <span>AnimalHealth</span>
                         </div>
-                        <p className="footerDescription">
+                        <p className={style.footerDescription}>
                             Connecting farmers with veterinary expertise for healthier animals and
                             sustainable agriculture.
                         </p>
-                        <div className="footerSocial">
-                            <a href="#"><i className="fab fa-facebook"></i></a>
-                            <a href="#"><i className="fab fa-twitter"></i></a>
-                            <a href="#"><i className="fab fa-instagram"></i></a>
-                            <a href="#"><i className="fab fa-telegram"></i></a>
+                        <div className={style.footerSocial}>
+                            <a href="#"><i className={`${fab} ${fa-facebook}`}></i></a>
+                            <a href="#"><i className={`${fab} ${fa-twitter}`}></i></a>
+                            <a href="#"><i className={`${fab} ${fa-instagram}`}></i></a>
+                            <a href="#"><i className={`${fab} ${fa-telegram}`}></i></a>
                         </div>
                     </div>
 
-                    <div className="footerSection">
+                    <div className={style.footerSection}>
                         <h4>Quick Links</h4>
-                        <ul className="footerLinks">
+                        <ul className={style.footerLinks}>
                             <li><a href="#features">Features</a></li>
                             <li><a href="#how-it-works">How It Works</a></li>
                             <li><a href="#testimonials">Testimonials</a></li>
@@ -37,9 +37,9 @@ function Footer() {
                         </ul>
                     </div>
 
-                    <div className="footerSection">
+                    <div className={style.footerSection}>
                         <h4>Resources</h4>
-                        <ul className="footerLinks">
+                        <ul className={style.footerLinks}>
                             <li><a href="#">Livestock Care Guide</a></li>
                             <li><a href="#">Disease Prevention</a></li>
                             <li><a href="#">Veterinary Resources</a></li>
@@ -48,19 +48,19 @@ function Footer() {
                         </ul>
                     </div>
 
-                    <div className="footerSection">
+                    <div className={style.footerSection}>
                         <h4>Contact</h4>
-                        <ul className="footerContact">
-                            <li><i className="fas fa-phone"></i> +251 911 234 567</li>
-                            <li><i className="fas fa-envelope"></i> support@animalhealth.et</li>
-                            <li><i className="fas fa-map-marker-alt"></i> Addis Ababa, Ethiopia</li>
+                        <ul className={style.footerContact}>
+                            <li><i className={`${fas} ${fa-phone}`}></i> +251 911 234 567</li>
+                            <li><i className={`${fas} ${fa-envelope}`}></i> support@animalhealth.et</li>
+                            <li><i className={`${fas} ${fa-map-marker-alt}`}></i> Addis Ababa, Ethiopia</li>
                         </ul>
                     </div>
                 </div>
 
-                <div className="footerBottom">
+                <div className={style.footerBottom}>
                     <p>© 2025 Animal Health Advisory System. All Rights Reserved.</p>
-                    <div className="footerLegal">
+                    <div className={style.footerLegal}>
                         <a href="#">Privacy Policy</a>
                         <a href="#">Terms of Service</a>
                         <a href="#">Cookie Policy</a>
