8139bfe9d2784d2567c27aead124791a6f7a9d9e
adane-desta <addonegn@gmail.com>
2026-01-17 14:25:21 +0300

    build role selet page

--- getvet/src/App.jsx
+++ getvet/src/App.jsx
@@ -2,11 +2,13 @@
 import './styles/global.css'
 import Home from "./pages/homePage/Home";
 import Login from './pages/auth/login/login';
+import RoleSelect from './pages/roleSelect/RoleSelect';
 function App(){
     return(
         <div>
             {/* <Home /> */}
-            <Login />
+            {/* <Login /> */}
+            <RoleSelect />
         </div>
     )
 }
