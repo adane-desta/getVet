dfd2e1c75b413730cea09377ca342324a926e173
adane-desta <addonegn@gmail.com>
2026-01-17 14:04:46 +0300

    style: login page

--- getvet/src/App.jsx
+++ getvet/src/App.jsx
@@ -1,10 +1,12 @@
 
 import './styles/global.css'
 import Home from "./pages/homePage/Home";
+import Login from './pages/auth/login/login';
 function App(){
     return(
         <div>
-            <Home />
+            {/* <Home /> */}
+            <Login />
         </div>
     )
 }
--- getvet/src/pages/auth/login/Login.jsx
+++ getvet/src/pages/auth/login/Login.jsx
@@ -12,30 +12,56 @@ function Login() {
       </div>
 
       <h1 id="welcomeText">Welcome Back</h1>
+
       <p id="descriptionText">
         Log in to manage your animals' health or assist farmers.
       </p>
 
       <div className={styles.userType}>
         <label htmlFor="farmerRadio">
-          <input type="radio" name="userType" value="farmers" id="farmerRadio" />
+          <input
+            type="radio"
+            name="userType"
+            value="farmers"
+            id="farmerRadio"
+          />
           <span id="farmerText">I'm a Farmer</span>
         </label>
+
         <label htmlFor="veterinarianRadio">
-          <input type="radio" name="userType" value="veterinarians" id="veterinarianRadio" />
+          <input
+            type="radio"
+            name="userType"
+            value="veterinarians"
+            id="veterinarianRadio"
+          />
           <span id="veterinarianText">I'm a Veterinarian</span>
         </label>
       </div>
 
       <form id="loginForm">
-        <input id="email" type="email" placeholder="Email" required />
-        <input id="password" type="password" placeholder="Password" required />
-        <button type="submit" id="loginBtn">Log In</button>
+        <input
+          id="email"
+          type="email"
+          placeholder="Email"
+          required
+        />
+        <input
+          id="password"
+          type="password"
+          placeholder="Password"
+          required
+        />
+        <button type="submit" id="loginBtn">
+          Log In
+        </button>
       </form>
 
       <p className={styles.signupLink}>
-        <span id="signupText">Don't have an account?</span>{" "}
-        <a href="join-us.html" id="joinText">Join Us</a>
+        <span id="signupText">Don't have an account?</span>{' '}
+        <a href="join-us.html" id="joinText">
+          Join Us
+        </a>
       </p>
     </div>
   );
