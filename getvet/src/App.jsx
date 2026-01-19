1f14c548ff0e1b3caf76718d31dd479d7710eacd
adane-desta <addonegn@gmail.com>
2026-01-19 09:54:00 +0300

    build and update login component

--- getvet/src/App.jsx
+++ getvet/src/App.jsx
@@ -2,12 +2,14 @@
 import './styles/global.css'
 import Signup from './pages/auth/signup/Signup';
 import Landing from './pages/homePage/Landing'
+import Login from './pages/auth/login/login';
 function App(){
     return(
 
         <div>
-        <Landing />
+        {/* <Landing /> */}
         {/* <Signup />  */}
+        <Login />
         </div>
     )
 }
