1f1236269c2332baccb6347ec57278bbe3a4c673
adane-desta <addonegn@gmail.com>
2026-01-19 10:22:49 +0300

    configuring router browsing

--- getvet/src/App.jsx
+++ getvet/src/App.jsx
@@ -1,17 +1,24 @@
 
 import './styles/global.css'
-import Signup from './pages/auth/signup/Signup';
+import {Routes , Route} from 'react-router-dom'
 import Landing from './pages/landing/Landing'
 import Login from './pages/auth/login/login';
+import Signup from './pages/auth/signup/Signup';
 import RoleSelect from './pages/roleSelect/RoleSelect';
+import Home from './pages/landing/Landing';
+
 function App(){
     return(
 
         <div>
-        {/* <Landing /> */}
-        {/* <Signup />  */}
-        {/* <Login /> */}
-        <RoleSelect />
+
+            <Routes >
+                <Route path='/' element={<Landing /> } />
+                <Route path='login' element={ <Login /> } />
+                <Route path='signup' element={ <Signup /> } />
+                <Route path='roleselect' element={ <RoleSelect /> } />
+                <Route path='home' element={ <Home /> } />
+            </Routes>
         </div>
     )
 }
