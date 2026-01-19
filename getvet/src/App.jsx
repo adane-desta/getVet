ec5b21ab4d73dac80319ca5db3418f398f2f1f2e
adane-desta <addonegn@gmail.com>
2026-01-19 10:09:27 +0300

    create roleSelect component and corresponding css module

--- getvet/src/App.jsx
+++ getvet/src/App.jsx
@@ -3,13 +3,15 @@ import './styles/global.css'
 import Signup from './pages/auth/signup/Signup';
 import Landing from './pages/landing/Landing'
 import Login from './pages/auth/login/login';
+import RoleSelect from './pages/roleSelect/RoleSelect';
 function App(){
     return(
 
         <div>
         {/* <Landing /> */}
         {/* <Signup />  */}
-        <Login />
+        {/* <Login /> */}
+        <RoleSelect />
         </div>
     )
 }
